85c2aa281606b4c96eb293e8ac0a3666805db248
tombflylee <tombflylee@hotmail.com>
2017-06-18 23:24:17 +0800

    0618

--- app/index.js
+++ app/index.js
@@ -1,4 +1,47 @@
-import { Calendar } from './components/calendar/index';
+/**
+ * test:calendar_test
+ */
+// import { Calendar } from './components/calendar/index';
 
-let calendar = new Calendar();
-calendar.init(document.getElementById('app'));
+// let calendar = new Calendar();
+// calendar.init(document.getElementById('app'));
+/************************************************************************/
+/**
+ * test:modal_test
+ */
+// import { Modal } from './components/modal-test/index'
+//
+// let header = document.createElement('p');
+// header.textContent = '确认信息';
+// let content = document.createElement('p');
+// content.textContent = '你确定吗？';
+// let modal = new Modal({
+// 	outerContain: document.getElementById('app'),
+// 	header: header,
+// 	content: content,
+// });
+// modal.init();
+// modal.bindEvent(document.getElementById('modal'));
+/************************************************************************/
+/**
+ * test:Form
+ */
+// import { Form } from './components/Form/index'
+//
+// let form = new Form(document.getElementById('form-container'));
+// form.init();
+// form.submit(document.getElementById('submit-btn'));
+/************************************************************************/
+import { create } from './Melon/Util/reference'
+import { Create } from './Melon/Util/csBuilderNew'
+var cClass= new Create('Calendar');
+cClass.init({className:'jioewjiow',size:'xxs',variants:[1,2]});
+cClass.setPart('HHH');
+cClass.removeVariants(1);
+cClass.clearVariants();
+var result = cClass.addStates({focus: true});
+console.log(result);
+
+
+console.log(create('Calendar')({disabled:true,states:{readOnly:true},hidden:true,variants:[4,5],className:'jioewjiow','size':'xxs'}).part('hehe').addStates({focus: open}).addVariants(1, 2, 3).removeVariants('xxs').build());
+//jioewjiow ui-calendar-hehe variant-2 variant-3 state-disabled state-focus
